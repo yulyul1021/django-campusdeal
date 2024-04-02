0e022e85fc0ec1e9acaeb52dd8f42951c3213f04
seonseon933 <seonseon933@gmail.com>
2024-04-02 20:29:06 +0900

    로그인,회원가입 템플릿 완성, config의 setting, urls, user의 model변경, 회원가입 완성

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    #path('login/', views.custom_login, name='custom_login'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
--- user/views.py
+++ user/views.py
@@ -1,26 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from django.contrib import messages
 
-'''
-def custom_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # 로그인 후 이동할 페이지로 redirect
-            return redirect('index')
-        else:
-            # 인증 실패 시 에러 처리
-             messages.error(request, 'Invalid username or password. Please try again.')
-             return render(request, 'user/login.html')
-    else:
-        # GET 요청 시 로그인 폼 보여주기
-        return render(request, 'user/login.html')
-'''
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
