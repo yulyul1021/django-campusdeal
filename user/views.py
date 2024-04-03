7dfde9aa4b162d04c5e7ec1240d52434d495ed28
yul <yulyul102101@gmail.com>
2024-04-03 21:03:31 +0900

    로그인 오류 수정

--- user/views.py
+++ user/views.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import UserForm
+from .models import User
 
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
-        print(form.username, form.password)
         if form.is_valid():
             # 사용자를 직접 저장하고, 패스워드를 해싱하여 저장
             user = form.save(commit=False)
+            user.set_password(request.POST["password"])
             user.deal_count = 0
             user.save()
 
             # 회원가입된 사용자로 로그인을 시도
-            print(user.password)
             login(request, user)
             return redirect('deal:index')
     else:
